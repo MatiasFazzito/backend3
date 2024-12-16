4d4fabecf8b07231e32014c7fbaca213e4ab9a89
Matias Fazzito <matyf11@hotmail.com>
2024-12-16 19:32:40 -0300

    coneccion a bd y formateo general del proyecto

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
+import express from 'express'
+import mongoose from 'mongoose'
+import cookieParser from 'cookie-parser'
+import usersRouter from './routes/users.router.js'
+import petsRouter from './routes/pets.router.js'
+import adoptionsRouter from './routes/adoption.router.js'
+import sessionsRouter from './routes/sessions.router.js'
 
-import usersRouter from './routes/users.router.js';
-import petsRouter from './routes/pets.router.js';
-import adoptionsRouter from './routes/adoption.router.js';
-import sessionsRouter from './routes/sessions.router.js';
+process.loadEnvFile()
 
-const app = express();
-const PORT = process.env.PORT||8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+const URLMongo = process.env.URLMONGO
 
-app.use(express.json());
-app.use(cookieParser());
+const app = express()
+const PORT = process.env.PORT || 8080
+const connection = mongoose.connect(URLMongo)
 
-app.use('/api/users',usersRouter);
-app.use('/api/pets',petsRouter);
-app.use('/api/adoptions',adoptionsRouter);
-app.use('/api/sessions',sessionsRouter);
+app.use(express.json())
+app.use(cookieParser())
 
-app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
+app.use('/api/users', usersRouter)
+app.use('/api/pets', petsRouter)
+app.use('/api/adoptions', adoptionsRouter)
+app.use('/api/sessions', sessionsRouter)
+app.use('/api/mocks')
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
