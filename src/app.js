2a63b164a35cc94b61fbbb76892ed5682496113c
Matias Fazzito <matyf11@hotmail.com>
2025-01-07 20:26:46 -0300

    configuracion de swagger y creacion de archivos yaml en sus respectivas carpetas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
+import specs from './utils/swagger.js'
+import swaggerUIExpress from "swagger-ui-express"
 import usersRouter from './routes/users.router.js'
 import petsRouter from './routes/pets.router.js'
 import adoptionsRouter from './routes/adoption.router.js'
@@ -14,6 +16,7 @@ const URLMongo = process.env.URLMONGO
 const app = express()
 const PORT = process.env.PORT || 8080
 const connection = mongoose.connect(URLMongo)
+app.use('/apidocs', swaggerUIExpress.serve, swaggerUIExpress.setup(specs))
 
 app.use(express.json())
 app.use(cookieParser())
---
+++ src/utils/swagger.js
@@ -0,0 +1,17 @@
+import swaggerJSDoc from "swagger-jsdoc"
+import __dirname from "./index.js"
+
+const swaggerOptions = {
+    definition:{
+        openapi:"3.0.1",
+        info:{
+            title:"Documentacion de aplicacion para adopcion de mascotas",
+            description:"API de adopcion de mascotas"
+        }
+    },
+    apis:[`${__dirname}/docs/**/*.yaml`]
+}
+
+const specs= swaggerJSDoc(swaggerOptions)
+
+export default specs
