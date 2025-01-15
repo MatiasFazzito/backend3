6390e466e128435efd6e7eaf7f18d6dbe6c7f255
Matias Fazzito <matyf11@hotmail.com>
2025-01-14 21:12:36 -0300

    creacion de dockerfile, carpeta docs y test con respectivos

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -2,16 +2,16 @@ import swaggerJSDoc from "swagger-jsdoc"
 import __dirname from "./index.js"
 
 const swaggerOptions = {
-    definition:{
-        openapi:"3.0.1",
-        info:{
-            title:"Documentacion de aplicacion para adopcion de mascotas",
-            description:"API de adopcion de mascotas"
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Documentacion de aplicacion para adopcion de mascotas",
+            description: "API de adopcion de mascotas"
         }
     },
-    apis:[`${__dirname}/docs/**/*.yaml`]
+    apis: [`${__dirname}/../docs/**/*.yaml`]
 }
 
-const specs= swaggerJSDoc(swaggerOptions)
+const specs = swaggerJSDoc(swaggerOptions)
 
 export default specs
